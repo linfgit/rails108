85d5142f8da28739257e7b4ef4096b11b5eb5d90
linfgit <linfgg1@gmail.com>
2017-01-30 21:24:26 +0800

    安装simple_form 简化bootstrap 表单，共三步，安装gem，设置，替换

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,3 +1,4 @@
 class Group < ActiveRecord::Base
   validates :title, presence: true #限制title 的内容不能为空
+  validates :description, presence: true #限制description 的内容不能为空 使用simple_form 这个gem后，直接在这里设置需要限制的内容，然后在修改控制，在html页面不用修改的情况下就可以提示错误（不知道这个观察对不对）
 end

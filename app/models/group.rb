19a23dd330ed618f73d180718948cbbb2f6d0c8b
linfgit <linfgg1@gmail.com>
2017-01-31 21:46:34 +0800

    建立群成员关系表

--- app/models/group.rb
+++ app/models/group.rb
@@ -3,4 +3,6 @@ class Group < ActiveRecord::Base
   #validates :description, presence: true #限制description 的内容不能为空 使用simple_form 这个gem后，直接在这里设置需要限制的内容，然后在修改控制，在html页面不用修改的情况下就可以提示错误（不知道这个观察对不对）
   belongs_to :user
   has_many :posts
+  has_many :group_relationships
+  has_many :members, through: :group_relationships, source: :user
 end

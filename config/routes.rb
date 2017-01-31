8e8ba44f99c4ce080254bafaeef7721531d2bd7a
linfgit <linfgg1@gmail.com>
2017-01-31 16:49:12 +0800

    建立可以在讨论组里发文章的架构，1、建立post 的model 架构 2、建立group、post、user 三者的关系。另外还建立了posts的控制，并设定了posts的路劲，在这里设定路劲后有执行rake routes ，为啥会执行这一步呢？是因为设定了嵌套的路径吗

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'groups#index' #前面的groups是代表文件夹，index是文件的意思，是这样吗？
-  resources :groups   #这个的意思是在http://localhost:3000/后新加一个路径吗？http://localhost:3000/groups ？
+  resources :groups do #这个的意思是在http://localhost:3000/后新加一个路径吗？http://localhost:3000/groups ？
+    resources :posts
+  end
 end

d3f6e40c11bde775283585830493eca61b382d75
linfgit <linfgg1@gmail.com>
2017-01-31 15:00:11 +0800

    实作登录系统，并限制新增讨论时必须登入，共3步，安装gem、产生会员系统必要的档案、添加控制，限制新增讨论时必须登入

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'groups#index' #前面的groups是代表文件夹，index是文件的意思，是这样吗？
   resources :groups   #这个的意思是在http://localhost:3000/后新加一个路径吗？http://localhost:3000/groups ？
